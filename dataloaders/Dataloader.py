6f6b199bb88082b0e5d18ced87c1c1b5d0c8d9ee
quyen228 <69098210+quyen228@users.noreply.github.com>
2022-08-08 21:35:25 +0700

    Update Dataloader.py

--- dataloaders/Dataloader.py
+++ dataloaders/Dataloader.py
@@ -14,25 +14,25 @@ class Dataloader(object):
         self.path_dataset = path
 
         print('initializing dataloader...')
-        self.init_phase('train')
-        self.init_phase('valid')
+        # self.init_phase('train')
+        # self.init_phase('valid')
         self.init_phase('test')
 
     def normalize_features(self, feats, get_moments=False, mean=None, std=None):
         reuse_mean = mean is not None and std is not None
-        if feats.shape[1] != 2048: # image features
-            raise ValueError('Features are expected to be 2048-dimensional (extracted with ResNet)')
+        # if feats.shape[1] != 2048: # image features
+        #     raise ValueError('Features are expected to be 2048-dimensional (extracted with ResNet)')
+        # else:
+        feats = np.array(feats.todense())
+        if reuse_mean:
+            mean_feats = mean
+            std_feats = std
         else:
-            feats = np.array(feats.todense())
-            if reuse_mean:
-                mean_feats = mean
-                std_feats = std
-            else:
-                mean_feats = feats.mean(axis=0)
-                std_feats = feats.std(axis=0)
+            mean_feats = feats.mean(axis=0)
+            std_feats = feats.std(axis=0)
 
-            # normalize
-            feats = (feats - mean_feats)/std_feats
+        # normalize
+        feats = (feats - mean_feats)/std_feats
         if get_moments:
             return feats, mean_feats, std_feats
         return feats
